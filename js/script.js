2173ffec35bbd558dc139a7bee0871ecfe737b10
tzitman <tamir.zitman@intel.com>
2025-04-07 18:17:44 +0300

    some images and splitting files

---
+++ js/script.js
@@ -0,0 +1,9 @@
+﻿const urlParams = new URLSearchParams(window.location.search);
+const mode = urlParams.get("mode");
+
+if (mode === "photos") {
+  const giftsSection = document.getElementById("gifts");
+  if (giftsSection) {
+    giftsSection.style.display = "none";
+  }
+}
